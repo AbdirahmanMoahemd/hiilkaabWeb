27b3f39c58d8add62b984b4d23f498d2bbd24ecb
unknown <kaah6978@gmail.com>
2023-02-21 18:43:49 +0300

    First commit

--- testhiilkaab/src/admin/pages/products/products.jsx
+++ testhiilkaab/src/admin/pages/products/products.jsx
@@ -55,7 +55,7 @@ const Products = () => {
     <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
       <Header
         category="Page"
-        title="Products"
+        title={`Products(${products && products.length})`}
         currentColor={currentColor}
         onClick={onClickFn}
         linktext="/addproducts"
