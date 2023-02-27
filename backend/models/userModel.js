0d6dbb2c0a09561cdcc669866bff845dced8f7d7
unknown <kaah6978@gmail.com>
2023-02-27 18:22:11 +0300

    First commit

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,103 +1,119 @@
-import mongoose from 'mongoose'
-import bcrypt  from 'bcryptjs'
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
-const userSchema = mongoose.Schema({
+const userSchema = mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     email: {
-        type: String,
-        required: true,
-        unique: true,
+      type: String,
+      required: true,
+      unique: true,
     },
     password: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     isAdmin: {
-        type: Boolean,
-        required: true,
-        default: false
+      type: Boolean,
+      required: true,
+      default: false,
     },
     phone: {
-        type: String,
+      type: String,
     },
     address: {
-        type: String,
-        default: ''
+      type: String,
+      default: "",
     },
     city: {
-        type: String,
-        default: ''
+      type: String,
+      default: "",
     },
     country: {
-        type: String,
-        default: ''
+      type: String,
+      default: "",
     },
     cart: [
-        {
-          product: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Product",
-            required: true,
+      {
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+          required: true,
+        },
+        images: [
+          {
+            type: String,
           },
-          quantity: {
+        ],
+        quantity: {
+          type: Number,
+          required: true,
+        },
+        sizes: [
+          {
+            type: String,
+          },
+        ],
+        colors: [
+          {
             type: Number,
-            required: true,
           },
-          sizes: [
-            {
-              type: String,
-            },
-          ],
-          colors: [
-            {
-              type: Number,
-            },
-          ],
+        ],
+      },
+    ],
+    wishlist: [
+      {
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+          required: true,
         },
-      ],
-      wishlist: [
-        {
-          product: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Product",
-            required: true,
-          },
+      },
+    ],
+    cartMeal: [
+      {
+        meal: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Meal",
+          required: true,
         },
-      ],
-      cartMeal: [
-        {
-          meal: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Meal",
-            required: true,
-          },
-          quantity: {
-            type: Number,
-            required: true,
-          },
+        quantity: {
+          type: Number,
+          required: true,
         },
-      ],
-    
-}, {
-    timestamps: true
-})
+        name: { type: String },
+        images: [
+          {
+            type: String,
+          },
+        ],
+        note: {
+          type: String,
+        },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
 
 userSchema.methods.matchPassword = async function (enteredPassword) {
-    return await bcrypt.compare(enteredPassword, this.password)
-}
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
-userSchema.pre('save', async function (next) {
-    if (!this.isModified('password')) {
-        next() 
-    }
-    
-    const salt = await bcrypt.genSalt(10)
-    this.password = await bcrypt.hash(this.password, salt)
-})
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
 
-const User = mongoose.model('User', userSchema)
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 
-export default User
+const User = mongoose.model("User", userSchema);
+
+export default User;
