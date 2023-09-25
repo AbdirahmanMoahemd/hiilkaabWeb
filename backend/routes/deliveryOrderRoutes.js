e531681b63dc680459cd00fdaa514c681113a8d6
AbdirahmanMoahemd <kaah6978@gmail.com>
2023-09-25 10:39:52 +0300

    test

--- backend/routes/deliveryOrderRoutes.js
+++ backend/routes/deliveryOrderRoutes.js
@@ -1,17 +1,23 @@
 import express from "express";
 import { admin, protect } from "../middlewares/authMiddleware.js";
-import { createDeliveryOrders, getDeliveryOrders, getDeliveryOrders2, getDeliveryOrdersById, updateDeliveryOrderByStatus } from "../controllers/deliveryController.js";
+import {
+  createDeliveryOrders,
+  getDeliveryOrders,
+  getDeliveryOrders2,
+  getDeliveryOrdersById,
+  getMyDeliveryOrders,
+  updateDeliveryOrderByStatus,
+} from "../controllers/deliveryController.js";
 
 const router = express.Router();
 
 router
   .route("/")
-  .get(protect,admin, getDeliveryOrders)
+  .get(protect, admin, getDeliveryOrders)
   .post(createDeliveryOrders);
 router.route("/get/app").get(protect, admin, getDeliveryOrders2);
+router.route("/get-my-delivery/:id").get(protect, getMyDeliveryOrders);
 router.route("/update/:id").put(protect, admin, updateDeliveryOrderByStatus);
-router
-  .route("/:id").get(getDeliveryOrdersById)
+router.route("/:id").get(getDeliveryOrdersById);
 
-
-  export default router;
+export default router;
